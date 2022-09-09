2896b6260a507785aa530cddfae4fcb472e1dbe7
Hassan Shahid <habibshahid2013@gmail.com>
2022-09-08 21:52:53 -0500

    First Spring Boot Application

--- src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -3,7 +3,7 @@ package com.udacity.jwdnd.c1.review;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+//First application comments
 @SpringBootApplication
 public class  ReviewApplication {
 
